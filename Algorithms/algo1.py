aef97884874f19ee079b44a934c4cb79fd74c7d4
Andrew Chang <andrewchang99@gmail.com>
2022-04-05 13:43:35 -0700

    no cap

--- Algorithms/algo1.py
+++ Algorithms/algo1.py
@@ -3,28 +3,31 @@ import datetime
 from getpass import getpass
 import alpaca_trade_api as tradeapi
 
-# Login Method
+# UNCOMMENT TO REMOVE TRACEBACKS
+#sys.tracebacklimit = 0
+
+# LOGIN METHOD
 def login(attempts=3):
     global alpaca
     print('Please enter account information.')
     try:
-        # Ask for API Login Info
+        # ASK FOR API ACCOUNT INFO
         BASE_URL = input('Base URL: ')
         ALPACA_API_KEY = input('API Key: ')
         ALPACA_SECRET_KEY = getpass('Secret Key: ')
-        # Instantiate REST API Connection
+        # INSTANTIATE REST API CONNECTION
         alpaca = tradeapi.REST(key_id=BASE_URL, secret_key=ALPACA_SECRET_KEY,
                             base_url=BASE_URL, api_version='v2')
     except KeyboardInterrupt:
-        # Abort Login
+        # ABORT LOGIN
         print('\n', 'Canceled by user. Exiting now.', sep='')
         sys.exit(0)
     except ValueError:
-        # Retry Login with same attempts
+        # RETRY LOGIN WITH SAME ATTEMPTS
         print('\n', 'Info must be in correct form. Try again.', '\n', sep='')
         login(attempts)
     except HTTPError:
-        # Retry Login with 1 less attempt
+        # RETRY LOGIN WITH 1 LESS ATTEMPT
         attempts -= 1
         if attempts < 1:
             print('No attempts remaining. Exiting now.')
@@ -34,10 +37,8 @@ def login(attempts=3):
             login(attempts)
     return BASE_URL, ALPACA_API_KEY, ALPACA_SECRET_KEY
 
-# Assign API Login Info
-sys.tracebacklimit = 0
+# ASSIGN API LOGIN INFO
 BASE_URL, ALPACA_API_KEY, ALPACA_SECRET_KEY = login()
-sys.tracebacklimit = None
 
-# Print account details
-alpaca.get_account()
+# PRINT ACCOUNT DETAILS
+print(alpaca.get_account())
