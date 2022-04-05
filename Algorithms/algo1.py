b61a3f67fd83e7f0286555f7940f3c1ddab81335
andrewchang98 <andrewchang99@gmail.com>
2022-04-04 23:17:34 -0700

    Added code.

---
+++ Algorithms/algo1.py
@@ -0,0 +1,22 @@
+import sys
+import datetime
+import getpass as getpass
+import alpaca_trade_api as tradeapi
+
+try:
+    # Ask for API Info
+    print('Please enter account information.')
+    BASE_URL = input('Base URL: ')
+    ALPACA_API_KEY = input('API Key: ')
+    ALPACA_SECRET_KEY = getpass('Secret Key: ' )
+except KeyboardInterrupt:
+    # Abort login
+    print()
+    print('Canceled by user. Exiting now.')
+    sys.exit(0)
+
+# Instantiate REST API Connection
+alpaca = tradeapi.REST(key_id=ALPACA_API_KEY, secret_key=ALPACA_SECRET_KEY,
+                    base_url=BASE_URL, api_version='v2')
+
+alpaca.get_account()
