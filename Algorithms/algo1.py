1b3e911d4f22c5baa828541a69b268cfdb48da61
andrewchang98 <andrewchang99@gmail.com>
2022-04-05 00:19:11 -0700

    Fixed bug

--- Algorithms/algo1.py
+++ Algorithms/algo1.py
@@ -5,12 +5,16 @@ import alpaca_trade_api as tradeapi
 
 # Login Method
 def login(attempts=3):
+    global alpaca
     try:
         # Ask for API Login Info
         print('Please enter account information.')
         BASE_URL = input('Base URL: ')
         ALPACA_API_KEY = input('API Key: ')
         ALPACA_SECRET_KEY = getpass('Secret Key: ')
+        # Instantiate REST API Connection
+        alpaca = tradeapi.REST(key_id=BASE_URL, secret_key=ALPACA_SECRET_KEY,
+                            base_url=BASE_URL, api_version='v2')
     except KeyboardInterrupt:
         # Abort Login
         print('\n', 'Canceled by user. Exiting now.', sep='')
@@ -34,10 +38,6 @@ def login(attempts=3):
 sys.tracebacklimit = 0
 BASE_URL, ALPACA_API_KEY, ALPACA_SECRET_KEY = login()
 sys.tracebacklimit = None
-
-# Instantiate REST API Connection
-alpaca = tradeapi.REST(key_id=BASE_URL, secret_key=ALPACA_SECRET_KEY,
-                    base_url=BASE_URL, api_version='v2')
 
 # Print account details
 alpaca.get_account()
