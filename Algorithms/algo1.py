d0a79f7ba32e46ed588326b83a393a0b2778106c
andrewchang98 <andrewchang99@gmail.com>
2022-04-05 00:28:57 -0700

    Fixed bugs

--- Algorithms/algo1.py
+++ Algorithms/algo1.py
@@ -6,9 +6,9 @@ import alpaca_trade_api as tradeapi
 # Login Method
 def login(attempts=3):
     global alpaca
+    print('Please enter account information.')
     try:
         # Ask for API Login Info
-        print('Please enter account information.')
         BASE_URL = input('Base URL: ')
         ALPACA_API_KEY = input('API Key: ')
         ALPACA_SECRET_KEY = getpass('Secret Key: ')
@@ -21,16 +21,16 @@ def login(attempts=3):
         sys.exit(0)
     except ValueError:
         # Retry Login with same attempts
-        print('\n', 'Information is incorrect. Try again.', sep='')
+        print('\n', 'Info must be in correct form. Try again.', '\n' sep='')
         login(attempts)
     except HTTPError:
         # Retry Login with 1 less attempt
-        print('\n', 'Information is incorrect. Try again.', sep='')
         attempts -= 1
         if attempts < 1:
-            print('No more attempts. Exiting now.')
+            print('No attempts remaining. Exiting now.')
         else:
-            print('{} attempt remaining.'.format(attempts))
+            print('\n', 'Incorrect account key(s). Try again.', sep='')
+            print('{} attempt remaining.'.format(attempts), '\n', sep='')
             login(attempts)
     return BASE_URL, ALPACA_API_KEY, ALPACA_SECRET_KEY
 
