67b013a602122ca0f01830bf507e2f5e32f758e9
Andrew Chang <andrewchang99@gmail.com>
2022-04-10 03:45:16 -0700

    typo

--- algorithms/utilities/login.py
+++ algorithms/utilities/login.py
@@ -5,9 +5,8 @@ import alpaca_trade_api as tradeapi
 
 # MAIN ROUTINE TO LOAD ACCOUNT INFO
 def login():
-
+    #
     APCA_API_BASE_URL = 'https://paper-api.alpaca.markets'
-
     # TRY TO LOAD PASSWORDS FILE
     rp = Printer(delay=0.05)
     rp.printer('Loading account info...')
@@ -34,18 +33,18 @@ def login():
             sys.exit(0)
     # CONNECT TO DEXCOM SHARE API
     try:
-        rp.printer('\nConnecting to Alpaca servers...')
+        rp.printer('\nConnecting to Alpaca servers...\n')
         alpaca = tradeapi.REST(APCA_API_KEY_ID, APCA_API_SECRET_KEY,
                                APCA_API_BASE_URL)
         stream = tradeapi.stream.Stream(APCA_API_KEY_ID, APCA_API_SECRET_KEY,
                                APCA_API_BASE_URL)
     except KeyboardInterrupt:
-        rp.printer('\nLogin failed. Please check username and password.')
+        rp.printer('Login failed. Please check username and password.')
         rp.printer('Exiting now.')
         sys.exit(0)
     else:
 
-        rp.printer('Logged in as {}\n'.format(APCA_API_KEY_ID))
+        rp.printer('Logged in as {}'.format(APCA_API_KEY_ID))
         account = alpaca.get_account()
-        rp.printer(account.status)
+        rp.printer('Your account is: {}'.format(account.status))
         return alpaca, stream
