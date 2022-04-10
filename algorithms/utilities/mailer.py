5848beb6593c9368b0b1f8bac8a973ae43d132d6
Andrew Chang <andrewchang99@gmail.com>
2022-04-09 23:23:23 -0700

    created mailer.py

---
+++ algorithms/utilities/mailer.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+mail_content = (message)
+#The mail addresses and password
+sender_address = 'alpacatradingbot1@gmail.com'
+sender_pass = '1mhzxtal1:46818'
+receiver_address = 'alpacatradinglog@gmail.com'
+#Setup the MIME
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+#The body and the attachments for the mail
+message.attach(MIMEText(mail_content, 'plain'))
+#Create SMTP session for sending the mail
+session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
+print('Mail Sent')
