23c36d0fd802c031a17c636b609ce52c3ae13028
Andrew Chang <andrewchang99@gmail.com>
2022-04-10 18:37:12 -0700

    added password example file

--- trading/login.py
+++ trading/login.py
@@ -2,25 +2,27 @@ import sys
 from prompter import prompter
 from slowprinter import Printer
 import alpaca_trade_api as tradeapi
+from twilio.rest import Client
+
 
 # MAIN ROUTINE TO LOAD ACCOUNT INFO
 def login(APCA_API_BASE_URL='https://paper-api.alpaca.markets'):
-    # TRY TO LOAD PASSWORDS FILE
+    # TRY TO IMPORT ALPACA KEY FROM 'passwords.py' FILE
     rp = Printer(delay=0.05)
     rp.printer('Loading account info...')
     try:
-        import passwords
+        from passwords import alpaca as alpaca_keys
     except ImportError:
         rp.printer('No account info found in ~/Trading')
         APCA_API_KEY_ID, APCA_API_SECRET_KEY = prompter(rp.printer)
     else:
-        # ASK TO LOGIN AS USER
-        rp.printer('Login as {} (Y/n)?'.format(passwords.alpaca['api_key']), end=' ')
+        # ASK TO LOGIN AS USER IF 'alpaca' IS FOUND IN 'passwords.py' FILE
+        rp.printer('Login as {} (Y/n)?'.format(alpaca_keys['acc_key']), end=' ')
         try:
             keystroke = input()
             if keystroke == 'y' or keystroke == 'Y':
-                APCA_API_KEY_ID = passwords.alpaca['api_key']
-                APCA_API_SECRET_KEY = passwords.alpaca['secret_key']
+                APCA_API_KEY_ID = alpaca_keys['acc_key']
+                APCA_API_SECRET_KEY = alpaca_keys['auth_key']
             elif keystroke == 'n' or keystroke == 'N':
                 APCA_API_KEY_ID, APCA_API_SECRET_KEY = prompter(rp.printer)
             else:
@@ -29,7 +31,7 @@ def login(APCA_API_BASE_URL='https://paper-api.alpaca.markets'):
         except KeyboardInterrupt:
             rp.printer('\nCancelled by user. Exiting now.')
             sys.exit(0)
-    # CONNECT TO DEXCOM SHARE API
+    # CONNECT AND ASSIGN ALPACA REST API AND STREAM
     try:
         rp.printer('\nConnecting to Alpaca servers...\n')
         alpaca = tradeapi.REST(APCA_API_KEY_ID, APCA_API_SECRET_KEY,
@@ -41,7 +43,6 @@ def login(APCA_API_BASE_URL='https://paper-api.alpaca.markets'):
         rp.printer('Exiting now.')
         sys.exit(0)
     else:
-
         rp.printer('Logged in as {}'.format(APCA_API_KEY_ID))
         account = alpaca.get_account()
         rp.printer('Your account is: {}\n'.format(account.status))
---
+++ trading/passwords_example.py
@@ -0,0 +1,16 @@
+"""
+PASSWORD EXAMPLE FILE FOR ALPACA TRADING LOGIN
+
+RENAME THIS FILE TO 'passwords.py'
+$ cd ~/Trading/trading
+$ sudo cp passwords_example.py passwords.py
+
+"""
+
+#PASSWORD INFORMATION FILE
+alpaca = {'acc_key':  '<APCA_API_KEY_ID>',
+          'auth_key': '<APCA_API_SECRET_KEY>'}
+
+twilio = {'acc_key':  '<TWILIO_ACCOUNT_SID>',
+          'auth_key': '<TWILIO_AUTH_TOKEN>',
+          'phone_num: '<TWILIO_PHONE_NUM>'}
