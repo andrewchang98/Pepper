29f84fe2e021cb92e352e1921d30f7027665e443
Andrew Chang <andrewchang99@gmail.com>
2022-04-10 17:41:28 -0700

    deleted utilities for now

--- algorithms/utilities/algo1.py
+++
@@ -1,7 +0,0 @@
-import sys
-import datetime
-import alpaca_trade_api as tradeapi
-from login import login
-
-# LOGIN AND ASSIGN ALPACA AND STREAM OBJECTS
-alpaca, stream = login(BASE_URL='https://paper-api.alpaca.markets')
