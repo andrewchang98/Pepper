1aa9e805f85b4508b511d7b9a02963e9f3b97ac8
Andrew Chang <andrewchang99@gmail.com>
2022-04-12 01:53:25 -0700

    Added RecursionError handling

--- trading/algo1.py
+++ trading/algo1.py
@@ -1,4 +1,10 @@
 import login
+from slowprinter import Printer
+
+slow = Printer()
 
 # LOGIN AND ASSIGN ALPACA AND STREAM OBJECTS
-bot = login.Connection()
+try:
+    bot = login.Connection()
+except RecursionError as error:
+    slow.printer(str(error))
