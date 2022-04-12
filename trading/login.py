c43ad1c808ac82e3e6cfe4b75984075123dcaa3f
Andrew Chang <andrewchang99@gmail.com>
2022-04-12 02:33:36 -0700

    fixed sms alert time format

--- trading/login.py
+++ trading/login.py
@@ -69,7 +69,7 @@ def sms_alert(twilio: Client,
               receiver: str,
               alert="!ALERT!",
               printer=print) -> None:
-    timestr = get_timestr('pst', date_format='%I:%M %p %w %d %Y')
+    timestr = get_timestr('pst', date_format='%I:%M %p %m/%d/%Y')
     body = twilio.messages.create(to=receiver,
                                   from_=sender,
                                   body=alert+' '+timestr)
