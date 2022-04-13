6728eceb30a708b499899dc6e83cd1e787e81746
Andrew Chang <andrewchang99@gmail.com>
2022-04-12 19:57:57 -0700

    Pepper can pray

--- pepper/utilities/Pepper.py
+++ pepper/utilities/Pepper.py
@@ -19,92 +19,50 @@ class Pepper:
         self.timestamp = get_datetime(tz)
         self.slow = Printer(50, disable_slowprinter)
 
+    # Very special ohyeppep Printer
+    def ohyep(self, message="Oh yep, Pep!") -> None:
+        self.slow.printer(message)
+
+    # Lock Pepper inside so she cannot hunt
     def lock(self) -> None:
         self.locked = True
-        self.slow.printer("Pepper cannot hunt.")
+        self.ohyep("Pep cannot hunt.")
 
+    # Unlock the door so Pepper can hunt outside
     def unlock(self) -> None:
         self.locked = False
-        self.slow.printer("Pepper can hunt!")
+        self.ohyep("Pep can hunt!")
 
-    def step(self):
-        pass
-
+    # Pepper pounces on prey (buy position)
     def pounce(self):
-        self.ohyep("Pepper is getting ready to pounce...")
-        self.ohyep("Pepper decided to pounce!")
-        self.ohyep("Pepper missed! Meow!")
+        self.ohyep("Pep is getting ready to pounce...")
+        self.ohyep("Pep decided to pounce!")
+        self.ohyep("Pep missed! Meow!")
         pass
 
+    # Pepper drops prey off at the door (sell position)
     def drop(self):
         pass
 
-    def hunt(self):
+    # Pepper steps without pouncing or dropping (hold position)
+    def step(self):
         pass
 
+    # Pepper hunts for prey in symbol_list
+    def hunt(self, symbol_list: list):
+        pass
+
+    # Pepper drops everything and prepares for the impending economic crisis
     def panic(self):
+        self.ohyep('Pep says: "Holy Jesus!"')
         pass
 
-    def ohyep(self, message="Oh yep, pep!") -> None:
-        self.slow.printer(message)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Connection:
-    def __init__(
-                 self,
-                 APCA_API_KEY_ID="https://paper-api.alpaca.markets",
-                 data_feed='sip',
-                 locked=False
-                ) -> None:
-        self.slow = Printer()
-        self.alpaca, \
-        self.stream, \
-        self.twilio = pep(APCA_API_KEY_ID,
-                            data_feed,
-                            disable_slowprinter=False,
-                            char_per_sec=50,
-                            max_attempts=3)
-        self.locked = locked
-        self.timestamp = get_timestr()
-        self.slow.printer("Connection successful: " + self.timestamp)
-
-    # Set lock attribute to True
-    def lock(self) -> None:
-        self.locked = True
-        self.slow.printer("Connection locked.")
-
-    # Set lock attribute to False
-    def unlock(self) -> None:
-        self.locked = False
-        self.slow.printer("Connection unlocked.")
-
-    # Return lock: bool attribute
-    def is_locked(self) -> bool:
-        return self.locked
-
-    # Return timestamp string
-    def get_start_time(self) -> str:
-        return self.timestamp
+    def pray(self):
+        self.ohyep("Hail, Mary, full of grace,")
+        self.ohyep("the Lord is with thee.")
+        self.ohyep("Blessed art thou amongst women")
+        self.ohyep("and blessed is the fruit of thy womb, Jesus.")
+        self.ohyep("Holy Mary, Mother of God,")
+        self.ohyep("pray for us sinners,")
+        self.ohyep("now and at the hour of our death.")
+        self.ohyep("Amen.")
