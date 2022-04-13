465a2ac8b0da526b710e34b5057b7b988c802db7
Andrew Chang <andrewchang99@gmail.com>
2022-04-13 02:11:08 -0700

    added Texter module

---
+++ pepper/utilities/Texter.py
@@ -0,0 +1,21 @@
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioException, TwilioRestException
+
+class Texter:
+    def __init__(
+                 self,
+                 acc_key: str,
+                 auth_key: str,
+                 phone_num: str,
+                 target_num: str
+                ):
+        self.client = Client(acc_key, auth_key)
+        self.phone_num = phone_num
+        self.target_num = target_num
+
+    def text(message) -> bool:
+        body = twilio.messages.create(
+                                      to=self.target_num,
+                                      from_=self.phone_num,
+                                      body=message
+                                     )
