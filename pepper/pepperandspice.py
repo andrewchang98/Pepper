e74b437ed53397f0bcdd3847ad4d983e1b2b5be8
Andrew Chang <andrewchang99@gmail.com>
2022-04-14 02:35:34 -0700

    typos

--- pepper/pepperandspice.py
+++ pepper/pepperandspice.py
@@ -1,6 +1,24 @@
 from datetime import datetime
 from utilities.Pepper import Pepper
 
-pep = Pepper()
+logger = logging.getLogger()
 
-pep.hunt({'GLD', 'SPY'})
+def main():
+
+if __name__ == '__main__':
+    import argparse
+
+    fmt = '%(asctime)s:%(filename)s:%(lineno)d:%(levelname)s:%(name)s:%(message)s'
+    logging.basicConfig(level=logging.INFO, format=fmt)
+    fh = logging.FileHandler('console.log')
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(logging.Formatter(fmt))
+    logger.addHandler(fh)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('symbols', nargs='+')
+    parser.add_argument('--lot', type=float, default=2000)
+
+    main(parser.parse_args())
+
+pep = Pepper()
